7dbda5ac8ad7d70421a56b9b066c11e65967d3a5
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2024-01-14 11:49:57 +0000

    contextAPI theme toggler

--- themeToggler/src/App.jsx
+++ themeToggler/src/App.jsx
@@ -1,22 +1,43 @@
+import { useEffect , useState} from 'react';
 import './App.css'
+import Card from './components/Card';
+import ThemeBtn from './components/ThemeBtn';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
 
+    const [themeMode, setThemeMode] = useState("light");
+
+    const lightTheme = () => {
+        setThemeMode("light");
+    }
+    const darkTheme = () => {
+        setThemeMode("dark");
+    }
+
+    // actual change in theme occurs in useEffect
+    useEffect(()=>{
+        const HTMLElement = document.querySelector("html");
+        // remove both themes(if present) and then set new theme, light or dark
+        HTMLElement.classList.remove("light", "dark");
+        HTMLElement.classList.add(themeMode);
+    }, [themeMode]);
+
   return (
-   
-  <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-              {/* themeBtn */}
-          </div>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                    <ThemeBtn />
+                </div>
 
-          <div className="w-full max-w-sm mx-auto">
-              {/* Card */}
-          </div>
-      </div>
-  </div>
-
-  )
+                <div className="w-full max-w-sm mx-auto">
+                    <Card />
+                </div>
+            </div>
+        </div>
+    </ThemeProvider>
+  );
 }
 
 export default App
