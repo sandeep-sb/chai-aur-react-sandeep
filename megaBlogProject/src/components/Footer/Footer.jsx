4b97bda2aa54432f0c299d0d512b9e67f4675374
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2024-01-23 12:22:20 +0000

    container, header, logoutBtn

--- megaBlogProject/src/components/Footer/Footer.jsx
+++ megaBlogProject/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../Logo";
+import {Logo} from "../Logo";
 import { Link } from 'react-router-dom'
 
 export const Footer = () => {
--- megaBlogProject/src/components/Header/Header.jsx
+++ megaBlogProject/src/components/Header/Header.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
+import {useSelector} from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import {Container, LogoutBtn, Logo} from "../index"
 
 export const Header = () => {
+    // const navigate = useNavigate();
+    const authStatus = useSelector((state) => state.auth.status);
+    const navItems = [
+        {
+            name: "All Posts",
+            slug: "/",
+            active: true
+        },
+        {
+            name: "login",
+            slug: "/login",
+            active: !authStatus
+        },
+        {
+            name: "Signup",
+            slug: "/signup",
+            active: !authStatus
+        },
+        {
+            name: "All Posts",
+            slug: "/allpost",
+            active: !authStatus
+        },
+        {
+            name: "Add Post",
+            slug: "/addpost",
+            active: !authStatus
+        },
+    ]
     return(
-        <>
-            <h1>Header</h1>
-        </>
+        <header>
+            <Container>
+                <nav className='flex'>
+                    <div className='mr-4'>
+                        {/* <Link to="/"> */}
+                            <Logo />
+                        {/* </Link> */}
+                    </div>
+                    <ul className='flex ml-auto'>
+                        {navItems.map((item) => 
+                        item.active ? (
+                            <li key={item.name}>
+                                <button
+                                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                                    // onClick={navigate(item.slug)}
+                                >
+                                    {item.name}
+                                </button>
+                            </li>
+                        ) : null
+                        )}
+                        {authStatus && 
+                        <li>
+                            <LogoutBtn />
+                        </li>}
+                    </ul>
+                </nav>
+            </Container>
+        </header>
     );
 }
